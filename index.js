40ac4468f6a48263d367491b4a1be348d38c86dc
Anfelesan <anfelesan@gmail.com>
2023-11-12 20:14:09 -0500

    Added project

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer();
+
+const io = require('socket.io')(server, {
+  cors: {origin: '*'}
+});
+
+io.on('connection', (socket) => {
+  console.log('Connected client');
+});
+
+server.listen(3002);
